732d923a06f377ed02869aba460fc266391a518c
JohannesHenz <johenz@protonmail.com>
2023-10-22 22:19:49 +0200

    twmailer function declaration

---
+++ Client_Server_Sample/twmailer-server.cpp
@@ -0,0 +1,385 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+#define BUF 1024
+#define PORT 6543
+
+///////////////////////////////////////////////////////////////////////////////
+
+int abortRequested = 0;
+int create_socket = -1;
+int new_socket = -1;
+
+///////////////////////////////////////////////////////////////////////////////
+
+void *clientCommunication(void *data);
+void signalHandler(int sig);
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+int main(void)
+{
+   socklen_t addrlen;
+   struct sockaddr_in address, cliaddress;
+   int reuseValue = 1;
+    const char* mailSpoolDir = "./mail-spool-directory";
+
+   ////////////////////////////////////////////////////////////////////////////
+
+   if (access(mailSpoolDir, F_OK) == -1) {
+    if (mkdir(mailSpoolDir, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == -1) {
+        perror("Error creating mail spool directory");
+        return -1;
+    }
+}
+
+
+   ////////////////////////////////////////////////////////////////////////////
+   // SIGNAL HANDLER
+   // SIGINT (Interrup: ctrl+c)
+   // https://man7.org/linux/man-pages/man2/signal.2.html
+   if (signal(SIGINT, signalHandler) == SIG_ERR)
+   {
+      perror("signal can not be registered");
+      return EXIT_FAILURE;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // CREATE A SOCKET
+   // https://man7.org/linux/man-pages/man2/socket.2.html
+   // https://man7.org/linux/man-pages/man7/ip.7.html
+   // https://man7.org/linux/man-pages/man7/tcp.7.html
+   // IPv4, TCP (connection oriented), IP (same as client)
+   if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+   {
+      perror("Socket error"); // errno set by socket()
+      return EXIT_FAILURE;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // SET SOCKET OPTIONS
+   // https://man7.org/linux/man-pages/man2/setsockopt.2.html
+   // https://man7.org/linux/man-pages/man7/socket.7.html
+   // socket, level, optname, optvalue, optlen
+   if (setsockopt(create_socket,
+                  SOL_SOCKET,
+                  SO_REUSEADDR,
+                  &reuseValue,
+                  sizeof(reuseValue)) == -1)
+   {
+      perror("set socket options - reuseAddr");
+      return EXIT_FAILURE;
+   }
+
+   if (setsockopt(create_socket,
+                  SOL_SOCKET,
+                  SO_REUSEPORT,
+                  &reuseValue,
+                  sizeof(reuseValue)) == -1)
+   {
+      perror("set socket options - reusePort");
+      return EXIT_FAILURE;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // INIT ADDRESS
+   // Attention: network byte order => big endian
+   memset(&address, 0, sizeof(address));
+   address.sin_family = AF_INET;
+   address.sin_addr.s_addr = INADDR_ANY;
+   address.sin_port = htons(PORT);
+
+   ////////////////////////////////////////////////////////////////////////////
+   // ASSIGN AN ADDRESS WITH PORT TO SOCKET
+   if (bind(create_socket, (struct sockaddr *)&address, sizeof(address)) == -1)
+   {
+      perror("bind error");
+      return EXIT_FAILURE;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // ALLOW CONNECTION ESTABLISHING
+   // Socket, Backlog (= count of waiting connections allowed)
+   if (listen(create_socket, 5) == -1)
+   {
+      perror("listen error");
+      return EXIT_FAILURE;
+   }
+
+   while (!abortRequested)
+   {
+      /////////////////////////////////////////////////////////////////////////
+      // ignore errors here... because only information message
+      // https://linux.die.net/man/3/printf
+      printf("Waiting for connections...\n");
+
+      /////////////////////////////////////////////////////////////////////////
+      // ACCEPTS CONNECTION SETUP
+      // blocking, might have an accept-error on ctrl+c
+      addrlen = sizeof(struct sockaddr_in);
+      if ((new_socket = accept(create_socket,
+                               (struct sockaddr *)&cliaddress,
+                               &addrlen)) == -1)
+      {
+         if (abortRequested)
+         {
+            perror("accept error after aborted");
+         }
+         else
+         {
+            perror("accept error");
+         }
+         break;
+      }
+
+      /////////////////////////////////////////////////////////////////////////
+      // START CLIENT
+      // ignore printf error handling
+      printf("Client connected from %s:%d...\n",
+             inet_ntoa(cliaddress.sin_addr),
+             ntohs(cliaddress.sin_port));
+      clientCommunication(&new_socket); // returnValue can be ignored
+      new_socket = -1;
+   }
+      /////////////////////////////////////////////////////////////////////////
+
+    enum COMMAND
+    {
+        SEND,
+        LIST,
+        READ,
+        DEL,
+        QUIT
+    };
+
+
+      /////////////////////////////////////////////////////////////////////////
+   // frees the descriptor
+   if (create_socket != -1)
+   {
+      if (shutdown(create_socket, SHUT_RDWR) == -1)
+      {
+         perror("shutdown create_socket");
+      }
+      if (close(create_socket) == -1)
+      {
+         perror("close create_socket");
+      }
+      create_socket = -1;
+   }
+
+   return EXIT_SUCCESS;
+}
+
+
+
+void handleSendCommand(int* current_socket, char buffer[])
+{
+//SEND\n
+//<Sender>\n
+//<Receiver>\n
+//<Subject (max. 80 chars)>\n
+//<message (multi-line; no length restrictions)\n>
+//.\n
+    
+}
+
+void handleListCommand(int* current_socket, char buffer[])
+{
+    //LIST\n
+    //<Username>\n
+    
+}
+
+void handleReadCommand(int* current_socket, char buffer[])
+{
+    //READ\n
+    //<Username>\n
+    //<Message-Number>\n
+    
+}
+
+void handleDelCommand(int* current_socket, char buffer[])
+{
+    //DEL\n
+    //<Username>\n
+    //<Message-Number>\n  
+    
+}
+
+
+void handleQuitCommand(int* current_socket, char buffer[])
+{
+    //QUIT\n
+}
+
+
+
+void *clientCommunication(void *data)
+{
+   char buffer[BUF];
+   int size;
+   int *current_socket = (int *)data;
+
+   ////////////////////////////////////////////////////////////////////////////
+   // SEND welcome message
+   strcpy(buffer, "Welcome to myserver!\r\nPlease enter your commands...\r\n");
+   if (send(*current_socket, buffer, strlen(buffer), 0) == -1)
+   {
+      perror("send failed");
+      return NULL;
+   }
+
+   do
+   {
+      /////////////////////////////////////////////////////////////////////////
+      // RECEIVE
+      size = recv(*current_socket, buffer, BUF - 1, 0);
+      if (size == -1)
+      {
+         if (abortRequested)
+         {
+            perror("recv error after aborted");
+         }
+         else
+         {
+            perror("recv error");
+         }
+         break;
+      }
+
+      if (size == 0)
+      {
+         printf("Client closed remote socket\n"); // ignore error
+         break;
+      }
+
+      // remove ugly debug message, because of the sent newline of client
+      if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n')
+      {
+         size -= 2;
+      }
+      else if (buffer[size - 1] == '\n')
+      {
+         --size;
+      }
+
+
+     if (strncmp(buffer, "SEND", 4) == 0) {
+            // Handle the SEND command
+            handleSendCommand(current_socket, buffer);
+        }
+        else if (strncmp(buffer, "LIST", 4) == 0) {
+            // Handle the LIST command
+            handleListCommand(current_socket, buffer);
+        }
+        else if (strncmp(buffer, "READ", 4) == 0) {
+            // Handle the READ command
+            handleReadCommand(current_socket, buffer);
+        }
+        else if (strncmp(buffer, "DEL", 3) == 0) {
+            // Handle the DEL command
+            handleDelCommand(current_socket, buffer);
+        }
+        else if (strncmp(buffer, "QUIT", 4) == 0) {
+            // Handle the QUIT command
+            handleQuitCommand(current_socket, buffer);
+        }
+        else {
+            // Handle unrecognized commands or provide an error message
+            // This part is essential for proper error handling
+
+            
+           // sendError(&current_socket, "Unrecognized command");
+        }
+
+
+
+
+
+      buffer[size] = '\0';
+      printf("Message received: %s\n", buffer); // ignore error
+
+      if (send(*current_socket, "OK", 3, 0) == -1)
+      {
+         perror("send answer failed");
+         return NULL;
+      }
+   } while (strcmp(buffer, "quit") != 0 && !abortRequested);
+
+   // closes/frees the descriptor if not already
+   if (*current_socket != -1)
+   {
+      if (shutdown(*current_socket, SHUT_RDWR) == -1)
+      {
+         perror("shutdown new_socket");
+      }
+      if (close(*current_socket) == -1)
+      {
+         perror("close new_socket");
+      }
+      *current_socket = -1;
+   }
+
+   return NULL;
+}
+
+
+
+
+void signalHandler(int sig)
+{
+   if (sig == SIGINT)
+   {
+      printf("abort Requested... "); // ignore error
+      abortRequested = 1;
+      /////////////////////////////////////////////////////////////////////////
+      // With shutdown() one can initiate normal TCP close sequence ignoring
+      // the reference count.
+      // https://beej.us/guide/bgnet/html/#close-and-shutdownget-outta-my-face
+      // https://linux.die.net/man/3/shutdown
+      if (new_socket != -1)
+      {
+         if (shutdown(new_socket, SHUT_RDWR) == -1)
+         {
+            perror("shutdown new_socket");
+         }
+         if (close(new_socket) == -1)
+         {
+            perror("close new_socket");
+         }
+         new_socket = -1;
+      }
+
+      if (create_socket != -1)
+      {
+         if (shutdown(create_socket, SHUT_RDWR) == -1)
+         {
+            perror("shutdown create_socket");
+         }
+         if (close(create_socket) == -1)
+         {
+            perror("close create_socket");
+         }
+         create_socket = -1;
+      }
+   }
+   else
+   {
+      exit(sig);
+   }
+}
